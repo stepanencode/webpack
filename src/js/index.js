1abc5cb478fce5c340e7975e482a905729abf11d
Oksana Stepanenko <stepanencode@gmail.com>
2025-01-16 13:54:06 +0100

    Create simple webpack settings dev & prod

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import '../styles/index.css';
+
+import {
+  onLoadEventHandler,
+  newTodoEventHandler,
+  removeTodoEventHandler,
+  toggleTodoEventListener,
+} from './event-handlers';
+
+window.addEventListener('load', onLoadEventHandler);
+document.addEventListener('change', function (event) {
+  if (event.target.classList.contains('new-todo')) {
+    newTodoEventHandler(event);
+  }
+});
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('delete')) {
+    removeTodoEventHandler(event);
+  }
+  if (event.target.classList.contains('real-checkbox')) {
+    toggleTodoEventListener(event);
+  }
+});
