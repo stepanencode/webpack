5705b625c6dc45df3df57de381fdb968a166eeb6
Oksana Stepanenko <stepanencode@gmail.com>
2025-01-20 12:05:38 +0100

    WP & postcss autoprefixer

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -53,7 +53,12 @@ module.exports = merge(common, {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
